c5d90d0f6dbccbc3d371ac107676955ca65090b9
MaximeBouton <maxim.bouton@gmail.com>
2019-10-07 16:04:50 -0700

    observation

--- src/observation.jl
+++ src/observation.jl
@@ -21,9 +21,12 @@ const OBS_DIRS = SVector(DSPos(0,1),
                         DSPos(-1,-1),
                         DSPos(-1,1))
 
+const OBS_QUAD = [:SW, :NW, :NE, :SE, :DET, :OUT]
+const N_OBS_PERFECT = 10
+const N_OBS_QUAD = 6
 
-
-POMDPs.observations(pomdp::DroneSurveillancePOMDP) = 1:N_OBS
+POMDPs.observations(pomdp::DroneSurveillancePOMDP{QuadCam}) = 1:N_OBS_QUAD
+POMDPs.observations(pomdp::DroneSurveillancePOMDP{PerfectCam}) = 1:N_OBS_PERFECT
 POMDPs.obsindex(pomdp::DroneSurveillancePOMDP, o::Int64) = o
 
 function POMDPs.observation(pomdp::DroneSurveillancePOMDP, a::Int64, s::DSState)
